3483a6c272d3376517acf8895f32233dcb305091
addagirigopikaspurthi <addagirispoorthi@gmail.com>
2026-06-28 08:24:37 +0530

    Implement Singleton Logger class in TestLogger

--- WEEK-1/DesignPrincipals&Patterns/EX-1/TestLogger.java
+++ WEEK-1/DesignPrincipals&Patterns/EX-1/TestLogger.java
@@ -1,19 +1,36 @@
+class Logger {
+
+    private static Logger instance;
+
+    private Logger() {
+        System.out.println("Logger object created");
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("Log: " + message);
+    }
+}
+
 public class TestLogger {
 
     public static void main(String[] args) {
 
         Logger logger1 = Logger.getInstance();
-
         Logger logger2 = Logger.getInstance();
 
         logger1.log("Application started");
         logger2.log("User logged in");
 
-        // Check whether both references point to same object
         if (logger1 == logger2) {
             System.out.println("Only one Logger instance exists.");
-        }
-        else {
+        } else {
             System.out.println("Multiple instances created.");
         }
     }
