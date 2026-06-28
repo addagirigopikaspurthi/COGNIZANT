e3ce3b0a40f56a7f2509d110b881707dd960a2d6
addagirigopikaspurthi <addagirispoorthi@gmail.com>
2026-06-28 08:23:56 +0530

    Update Logger.java

--- WEEK-1/DesignPrincipals&Patterns/EX-1/Logger.java
+++ WEEK-1/DesignPrincipals&Patterns/EX-1/Logger.java
@@ -7,15 +7,26 @@ class Logger {
     }
 
     public static Logger getInstance() {
-
         if (instance == null) {
             instance = new Logger();
         }
-
         return instance;
     }
 
     public void log(String message) {
         System.out.println("Log: " + message);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        logger1.log("Application Started");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("User Logged In");
+
+        System.out.println(logger1 == logger2);
     }
 }
